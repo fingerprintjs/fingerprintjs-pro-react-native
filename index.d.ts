a07f9d5965fd8c1c2e683523b478614fb0ef5c0a
stereodenis <stereodenis@gmail.com>
2022-02-03 12:38:05 +0300

    add region

--- index.d.ts
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-declare class FingerprintjsPro extends React.Component {
-    static init(apiKey: string): void;
-    static getVisitorId(): Promise<string>;
-}
-export default FingerprintjsPro;
