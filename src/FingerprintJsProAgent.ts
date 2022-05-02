285a73514708be78183a669c6ee20ea975391bf8
Ilya Taratukhin <darkilfa@gmail.com>
2022-05-02 15:27:56 +0300

    chore: make agent compatible with mocks

--- src/FingerprintJsProAgent.ts
+++ src/FingerprintJsProAgent.ts
@@ -1,14 +1,12 @@
 import { NativeModules } from "react-native";
 import { Region } from "./types";
 
-const { RNFingerprintjsPro } = NativeModules;
-
 type VisitorId = string;
 
 export class FingerprintJsProAgent {
   constructor(apiKey: string, region?: Region, endpointUrl?: string) {
     try {
-      RNFingerprintjsPro.init(apiKey, region, endpointUrl);
+      NativeModules.RNFingerprintjsPro.init(apiKey, region, endpointUrl);
     } catch (e) {
       console.error("RNFingerprintjsPro init error: ", e);
     }
@@ -16,7 +14,7 @@ export class FingerprintJsProAgent {
 
   public getVisitorId(): Promise<VisitorId> {
     try {
-      return RNFingerprintjsPro.getVisitorId();
+      return NativeModules.RNFingerprintjsPro.getVisitorId();
     } catch (e) {
       console.error("RNFingerprintjsPro getVisitorId error: ", e);
       throw new Error("RNFingerprintjsPro getVisitorId error: " + e);
