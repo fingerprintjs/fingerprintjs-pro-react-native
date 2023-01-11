51a6b45a876e25d0cec272a8b9437030ca677727
Ilya Taratukhin <ilya.taratukhin@fingerprint.com>
2023-01-11 18:57:48 +0100

    feat: update agents to add proxy integrations support CloudFlare for iOS and Android Cloudfront for iOS

--- TestProject/App.js
+++ TestProject/App.js
@@ -4,11 +4,13 @@ import { SafeAreaView, StatusBar, useColorScheme } from 'react-native'
 import { Colors } from 'react-native/Libraries/NewAppScreen'
 import { FingerprintJsProProvider } from '@fingerprintjs/fingerprintjs-pro-react-native'
 import { Visitor } from './src/Visitor'
-import { PUBLIC_API_KEY } from '@env'
+import { PUBLIC_API_KEY, REGION, ENDPOINT } from '@env'
 
 const App = () => {
   const isDarkMode = useColorScheme() === 'dark'
   const apiKey = PUBLIC_API_KEY
+  const region = REGION
+  const endpoint = ENDPOINT
 
   const backgroundStyle = {
     backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
@@ -17,7 +19,7 @@ const App = () => {
   }
 
   return (
-    <FingerprintJsProProvider apiKey={apiKey} extendedResponseFormat={true}>
+    <FingerprintJsProProvider apiKey={apiKey} extendedResponseFormat={true} region={region} endpointUrl={endpoint}>
       <SafeAreaView style={backgroundStyle}>
         <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
         <Visitor />
