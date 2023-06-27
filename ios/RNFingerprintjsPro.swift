e544d7e680f945ac46e085bfa88e0858cdb494f8
Ilya Taratukhin <ilya.taratukhin@fingerprint.com>
2023-06-27 17:16:41 +0200

    fix: mark library as not required in MainQueue #79

--- ios/RNFingerprintjsPro.swift
+++ ios/RNFingerprintjsPro.swift
@@ -15,6 +15,10 @@ class RNFingerprintjsPro: NSObject {
         super.init()
     }
 
+    @objc static func requiresMainQueueSetup() -> Bool {
+        return false
+    }
+
     @objc(init:region:endpoint:extendedResponseFormat:pluginVersion:)
     public required init(_ apiToken: String, _ region: String? = "us", _ endpoint: String? = nil, _ extendedResponseFormat: Bool = false, _ pluginVersion: String) {
         let region = RNFingerprintjsPro.parseRegion(region, endpoint: endpoint)
