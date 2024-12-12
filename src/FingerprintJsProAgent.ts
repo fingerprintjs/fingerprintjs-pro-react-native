b411ac68d41060b04a5ca306b9a5e0dbf7d1f317
Necip Allef <necipallef@gmail.com>
2024-12-12 07:43:06 +0300

    docs: jsdoc for functions

--- src/FingerprintJsProAgent.ts
+++ src/FingerprintJsProAgent.ts
@@ -5,7 +5,16 @@ import * as packageInfo from '../package.json'
 
 type VisitorId = string
 
+/**
+ *
+ * @group API Client approach
+ */
 export class FingerprintJsProAgent {
+  /**
+   * Initialises FingerprintJS Pro Agent with certain settings
+   *
+   * @param params
+   */
   private requestOptions: RequestOptions = {}
 
   constructor({
@@ -31,6 +40,13 @@ export class FingerprintJsProAgent {
     }
   }
 
+  /**
+   * Returns visitor identifier based on the request options {@link https://dev.fingerprint.com/docs/native-android-integration#get-the-visitor-identifier | more info in the documentation page}
+   *
+   * @param tags is a customer-provided value or an object that will be saved together with the analysis event and will be returned back to you in a webhook message or when you search for the visit in the server API. {@link https://dev.fingerprint.com/docs/js-agent#tag | more info in the documentation page}
+   * @param linkedId  is a way of linking current analysis event with a custom identifier. This will allow you to filter visit information when using the Server API {@link https://dev.fingerprint.com/docs/js-agent#linkedid | more info in the documentation page}
+   * @param options todo
+   */
   public async getVisitorId(tags?: Tags, linkedId?: string, options?: RequestOptions): Promise<VisitorId> {
     try {
       const timeout = options?.timeout ?? this.requestOptions.timeout
@@ -44,6 +60,15 @@ export class FingerprintJsProAgent {
     }
   }
 
+  /**
+   * Returns visitor identification data based on the request options {@link https://dev.fingerprint.com/docs/native-android-integration#get-the-visitor-identifier | more info in the documentation page}
+   *
+   * Provide `extendedResponseFormat` option in the {@link constructor} to get response in the {@link https://dev.fingerprint.com/docs/native-android-integration#response-format | extended format}
+   *
+   * @param tags is a customer-provided value or an object that will be saved together with the analysis event and will be returned back to you in a webhook message or when you search for the visit in the server API. {@link https://dev.fingerprint.com/docs/js-agent#tag | more info in the documentation page}
+   * @param linkedId  is a way of linking current analysis event with a custom identifier. This will allow you to filter visit information when using the Server API {@link https://dev.fingerprint.com/docs/js-agent#linkedid | more info in the documentation page}
+   * @param options todo
+   */
   public async getVisitorData(tags?: Tags, linkedId?: string, options?: RequestOptions): Promise<VisitorData> {
     try {
       const timeout = options?.timeout ?? this.requestOptions.timeout
