5015345dbdfb19e6d3263cc09f52baff6d7f1cd3
Necip Allef <necipallef@gmail.com>
2024-12-12 20:26:36 +0300

    feat: ios bridge refactor, added new methods with timeout

--- ios/RNFingerprintjsPro.m
+++ ios/RNFingerprintjsPro.m
@@ -2,8 +2,38 @@
 
 @interface RCT_EXTERN_MODULE(RNFingerprintjsPro, NSObject)
 
-RCT_EXTERN_METHOD(init:(NSString *)apiToken region:(NSString *)region endpoint:(NSString *)endpointUrl endpointFallbacks:(NSArray<NSString *> *)fallbackEndpointUrls extendedResponseFormat:(BOOL)extendedResponseFormat pluginVersion:(NSString *)pluginVersion)
-RCT_EXTERN_METHOD(getVisitorId:(NSDictionary *)tags linkedId:(NSString *)linkedId timeout:(NSNumber * _Nullable)timeout resolve:(RCTPromiseResolveBlock)resolve rejecter:(RCTPromiseRejectBlock)reject)
-RCT_EXTERN_METHOD(getVisitorData:(NSDictionary *)tags linkedId:(NSString *)linkedId timeout:(NSNumber * _Nullable)timeout resolve:(RCTPromiseResolveBlock)resolve rejecter:(RCTPromiseRejectBlock)reject)
+RCT_EXTERN_METHOD(init:(NSString *)apiToken
+  region:(NSString *)region
+  endpoint:(NSString *)endpointUrl
+  endpointFallbacks:(NSArray<NSString *> *)fallbackEndpointUrls
+  extendedResponseFormat:(BOOL)extendedResponseFormat
+  pluginVersion:(NSString *)pluginVersion
+)
+
+RCT_EXTERN_METHOD(getVisitorId:(NSDictionary *)tags
+  linkedId:(NSString *)linkedId
+  resolve:(RCTPromiseResolveBlock)resolve
+  rejecter:(RCTPromiseRejectBlock)reject
+)
+
+RCT_EXTERN_METHOD(getVisitorIdWithTimeout:(NSDictionary *)tags
+  linkedId:(NSString *)linkedId
+  timeout:(nonnull NSNumber)timeout
+  resolve:(RCTPromiseResolveBlock)resolve
+  rejecter:(RCTPromiseRejectBlock)reject
+)
+
+RCT_EXTERN_METHOD(getVisitorData:(NSDictionary *)tags
+  linkedId:(NSString *)linkedId
+  resolve:(RCTPromiseResolveBlock)resolve
+  rejecter:(RCTPromiseRejectBlock)reject
+)
+
+RCT_EXTERN_METHOD(getVisitorDataWithTimeout:(NSDictionary *)tags
+  linkedId:(NSString *)linkedId
+  timeout:(nonnull NSNumber)timeout
+  resolve:(RCTPromiseResolveBlock)resolve
+  rejecter:(RCTPromiseRejectBlock)reject
+)
 
 @end
