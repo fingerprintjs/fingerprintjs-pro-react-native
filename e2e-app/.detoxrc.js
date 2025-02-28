41559c6e576867669d8c8a2a9978370eed253699
Przemysław Żydek <przemyslawzydek@gmail.com>
2025-02-28 13:27:21 +0100

    build: update artifacts config

--- e2e-app/.detoxrc.js
+++ e2e-app/.detoxrc.js
@@ -2,82 +2,86 @@
 module.exports = {
   testRunner: {
     args: {
-      '$0': 'jest',
-      config: 'e2e/jest.config.js'
+      $0: 'jest',
+      config: 'e2e/jest.config.js',
     },
     jest: {
-      setupTimeout: 120000
-    }
+      setupTimeout: 120000,
+    },
+  },
+  artifacts: {
+    rootDir: 'e2e-app/artifacts',
+    uiHierarchy: true,
   },
   apps: {
     'ios.debug': {
       type: 'ios.app',
       binaryPath: 'ios/build/Build/Products/Debug-iphonesimulator/e2eapp.app',
-      build: 'xcodebuild -workspace ios/e2eapp.xcworkspace -scheme e2eapp -configuration Debug -sdk iphonesimulator -derivedDataPath ios/build'
+      build:
+        'xcodebuild -workspace ios/e2eapp.xcworkspace -scheme e2eapp -configuration Debug -sdk iphonesimulator -derivedDataPath ios/build',
     },
     'ios.release': {
       type: 'ios.app',
       binaryPath: 'ios/build/Build/Products/Release-iphonesimulator/e2eapp.app',
-      build: 'xcodebuild -workspace ios/e2eapp.xcworkspace -scheme e2eapp -configuration Release -sdk iphonesimulator -derivedDataPath ios/build'
+      build:
+        'xcodebuild -workspace ios/e2eapp.xcworkspace -scheme e2eapp -configuration Release -sdk iphonesimulator -derivedDataPath ios/build',
     },
     'android.debug': {
       type: 'android.apk',
       binaryPath: 'android/app/build/outputs/apk/debug/app-debug.apk',
       build: 'cd android && ./gradlew assembleDebug assembleAndroidTest -DtestBuildType=debug',
-      reversePorts: [
-        8081
-      ]
+      reversePorts: [8081],
     },
     'android.release': {
       type: 'android.apk',
       binaryPath: 'android/app/build/outputs/apk/release/app-release.apk',
-      build: 'cd android && ./gradlew assembleRelease assembleAndroidTest -DtestBuildType=release'
-    }
+      build: 'cd android && ./gradlew assembleRelease assembleAndroidTest -DtestBuildType=release',
+    },
   },
   devices: {
     simulator: {
       type: 'ios.simulator',
       device: {
-        type: 'iPhone 16'
-      }
+        type: 'iPhone 16',
+      },
     },
     attached: {
       type: 'android.attached',
       device: {
-        adbName: '.*'
-      }
+        adbName: '.*',
+      },
     },
     emulator: {
       type: 'android.emulator',
       device: {
-        avdName: 'Pixel_9_Pro_XL_API_35'
-      }
-    }
+        avdName: 'Pixel_9_Pro_XL_API_35',
+      },
+    },
   },
   configurations: {
     'ios.sim.debug': {
       device: 'simulator',
-      app: 'ios.debug'
+      app: 'ios.debug',
     },
     'ios.sim.release': {
       device: 'simulator',
-      app: 'ios.release'
+      app: 'ios.release',
     },
     'android.att.debug': {
       device: 'attached',
-      app: 'android.debug'
+      app: 'android.debug',
     },
     'android.att.release': {
       device: 'attached',
-      app: 'android.release'
+      app: 'android.release',
     },
     'android.emu.debug': {
       device: 'emulator',
-      app: 'android.debug'
+      app: 'android.debug',
     },
     'android.emu.release': {
       device: 'emulator',
-      app: 'android.release'
-    }
-  }
-};
+      app: 'android.release',
+    },
+  },
+}
