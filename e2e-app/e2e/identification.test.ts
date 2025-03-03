6830e9f9f48479bec8572deb954d1e6a12dcf2a0
Przemysław Żydek <przemyslawzydek@gmail.com>
2025-03-03 16:19:08 +0100

    test: add first identification test

---
+++ e2e-app/e2e/identification.test.ts
@@ -0,0 +1,29 @@
+import { by, device, element, waitFor } from 'detox'
+import type { LaunchArgs } from '@/App'
+import { testIds } from '@/e2e/ids'
+
+describe('React Native Identification on US Region', () => {
+  beforeAll(async () => {
+    await device.launchApp({
+      launchArgs: {
+        apiKey: process.env.MAXIMUM_US_DEFAULT_PUBLIC_KEY,
+        region: 'us',
+      } as LaunchArgs,
+    })
+  })
+
+  beforeEach(async () => {
+    await device.reloadReactNative()
+  })
+
+  it('should return visitor data', async () => {
+    await element(by.id(testIds.getData)).tap()
+    await waitFor(element(by.id(testIds.data)))
+      .toExist()
+      .withTimeout(10_000)
+
+    const attributes = await element(by.id(testIds.data)).getAttributes()
+
+    console.log(attributes)
+  })
+})
