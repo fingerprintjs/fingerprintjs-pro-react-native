9a94b8f4f78bb5c4abb6f922f1c8695c96234f62
Przemysław Żydek <przemyslawzydek@gmail.com>
2025-03-03 17:00:49 +0100

    test: use correct import paths

--- e2e-app/e2e/identification.test.ts
+++ e2e-app/e2e/identification.test.ts
@@ -1,12 +1,12 @@
 import { by, device, element, waitFor } from 'detox'
 import type { LaunchArgs } from '@/App'
-import { testIds } from '@/e2e/ids'
+import { testIds } from './ids'
 
 describe('React Native Identification on US Region', () => {
   beforeAll(async () => {
     await device.launchApp({
       launchArgs: {
-        apiKey: process.env.MAXIMUM_US_DEFAULT_PUBLIC_KEY,
+        apiKey: process.env.MAXIMUM_US_DEFAULT_PUBLIC_KEY ?? '',
         region: 'us',
       } as LaunchArgs,
     })
