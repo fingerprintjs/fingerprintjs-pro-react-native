606f2369f265217acf7c9d993b147420b082d209
Przemysław Żydek <przemyslawzydek@gmail.com>
2025-03-18 13:23:52 +0100

    test: use node SDK for verifying event

--- e2e-app/e2e/identification.test.ts
+++ e2e-app/e2e/identification.test.ts
@@ -1,37 +1,87 @@
 import { by, device, element, waitFor } from 'detox'
-import { expect as jestExpect } from '@jest/globals'
+import { expect } from '@jest/globals'
 import type { LaunchArgs } from '@/App'
 import { testIds } from './ids'
+import { FingerprintJsServerApiClient, Region } from '@fingerprintjs/fingerprintjs-pro-server-api'
 
 const VISITOR_ID_REGEX = /^[a-zA-Z\d]{20}$/
 
-describe('React Native Identification on US Region', () => {
-  beforeAll(async () => {
-    const apiKey = process.env.MINIMUM_US_DEFAULT_PUBLIC_KEY
+async function identify() {
+  await element(by.id(testIds.getData)).tap()
+  await waitFor(element(by.id(testIds.data)))
+    .toExist()
+    .withTimeout(10_000)
 
-    if (!apiKey) {
-      throw new Error('MINIMUM_US_DEFAULT_PUBLIC_KEY is required to run this test')
-    }
+  const attributes = (await element(by.id(testIds.data)).getAttributes()) as Record<string, any>
+  const text = attributes?.text ?? attributes?.label
 
-    await device.launchApp({
-      newInstance: true,
-      launchArgs: {
-        apiKey,
-        region: 'us',
-      } as LaunchArgs,
+  return JSON.parse(text) as { visitorId: string; requestId: string }
+}
+
+describe.each([['us', process.env.MINIMUM_US_DEFAULT_PUBLIC_KEY, process.env.MINIMUM_US_DEFAULT_PRIVATE_KEY]] as const)(
+  'React Native Identification on %s Region',
+  (region, apiKey, privateApiKey) => {
+    let client: FingerprintJsServerApiClient
+
+    beforeAll(async () => {
+      if (!apiKey) {
+        throw new Error('apiKey is required to run this test')
+      }
+
+      if (!privateApiKey) {
+        throw new Error('privateApiKey is required to run this test')
+      }
+
+      let serverRegion: Region
+
+      switch (region) {
+        case 'us':
+        default:
+          serverRegion = Region.Global
+          break
+      }
+
+      client = new FingerprintJsServerApiClient({
+        apiKey: privateApiKey,
+        region: serverRegion,
+      })
     })
-  })
 
-  it('should return visitor data', async () => {
-    await element(by.id(testIds.getData)).tap()
-    await waitFor(element(by.id(testIds.data)))
-      .toExist()
-      .withTimeout(10_000)
+    it('should return visitor data', async () => {
+      await device.launchApp({
+        newInstance: true,
+        launchArgs: {
+          apiKey,
+          region,
+        } as LaunchArgs,
+      })
 
-    const attributes = (await element(by.id(testIds.data)).getAttributes()) as Record<string, any>
+      const json = await identify()
+      expect(json.visitorId).toMatch(VISITOR_ID_REGEX)
+    })
 
-    const text = attributes?.text ?? attributes?.label
-    const json = JSON.parse(text)
-    jestExpect(json.visitorId).toMatch(VISITOR_ID_REGEX)
-  })
-})
+    it('should return visitor data with linkedId and tag', async () => {
+      const linkedId = `${Date.now()}-rn-test`
+      const tags = {
+        'react-native-test': true,
+      }
+
+      await device.launchApp({
+        newInstance: true,
+        launchArgs: {
+          apiKey,
+          region,
+          linkedId,
+          tags,
+        } as LaunchArgs,
+      })
+
+      const json = await identify()
+      expect(json.visitorId).toMatch(VISITOR_ID_REGEX)
+
+      const event = await client.getEvent(json.requestId)
+      expect(event.products.identification?.data?.linkedId).toEqual(linkedId)
+      expect(event.products.identification?.data?.tag).toEqual(tags)
+    })
+  }
+)
