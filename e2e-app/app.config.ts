80e6d77a203970c00ea4ed720ed9277afef04375
Przemysław Żydek <przemyslawzydek@gmail.com>
2025-04-01 09:47:16 +0200

    refactor: use app.config.ts

---
+++ e2e-app/app.config.ts
@@ -0,0 +1,103 @@
+import { ExpoConfig } from '@expo/config'
+import { dependencies } from './package.json'
+import { ExtraIosPodDependency } from 'expo-build-properties/build/pluginConfig'
+import * as semver from 'semver'
+
+const rnVersion = semver.parse(dependencies['react-native'])
+if (!rnVersion) {
+  throw new Error(`Could not parse react-native version: ${dependencies['react-native']}`)
+}
+
+let newArch = false
+const disableNewArch = () => {
+  newArch = false
+}
+
+const androidBuildProperties = {
+  minSdkVersion: 24,
+  compileSdkVersion: 35,
+  targetSdkVersion: 35,
+  buildToolsVersion: '34.0.0',
+  extraMavenRepos: ['https://maven.fpregistry.io/releases'],
+} as Record<string, any>
+const stripExtraBuildProperties = () => {
+  delete androidBuildProperties.compileSdkVersion
+  delete androidBuildProperties.targetSdkVersion
+  delete androidBuildProperties.buildToolsVersion
+}
+
+/**
+ * Holds metadata for specific versions of React Native, including callbacks
+ * for version-specific adjustments or operations.
+ *
+ * @type {Record<string, { callbacks: (() => void)[] }>}
+ *
+ * - The key represents the version of React Native.
+ * - The value is an object containing:
+ *   - `callbacks`: An array of functions to execute in the context of the respective React Native version.
+ */
+const reactNativeMetadata: Record<string, { callbacks: (() => void)[] }> = {
+  0.73: {
+    callbacks: [disableNewArch, stripExtraBuildProperties],
+  },
+  0.74: {
+    callbacks: [disableNewArch, stripExtraBuildProperties],
+  },
+  0.75: {
+    callbacks: [disableNewArch, stripExtraBuildProperties],
+  },
+  0.76: {
+    callbacks: [stripExtraBuildProperties],
+  },
+}
+const metadataKey = rnVersion.major + '.' + rnVersion.minor
+if (reactNativeMetadata[metadataKey]?.callbacks?.length) {
+  console.info(`Applying metadata for React Native ${metadataKey}.`)
+  reactNativeMetadata[metadataKey].callbacks.forEach((callback) => callback())
+}
+
+const extraPods = [] as ExtraIosPodDependency[]
+const usesLocalPackage = dependencies['@fingerprintjs/fingerprintjs-pro-react-native'] === '../'
+if (usesLocalPackage) {
+  console.info(
+    'Pushing RNFingerprintjsPro as extra pod, because app uses local version of @fingerprintjs/fingerprintjs-pro-react-native.'
+  )
+  extraPods.push({
+    name: 'RNFingerprintjsPro',
+    path: '../node_modules/@fingerprintjs/fingerprintjs-pro-react-native',
+  })
+}
+
+const config: ExpoConfig = {
+  name: 'E2EApp',
+  slug: 'e2eapp',
+  version: '1.0.0',
+  orientation: 'portrait',
+  icon: './assets/images/icon.png',
+  scheme: 'e2eapp',
+  newArchEnabled: newArch,
+  splash: {
+    backgroundColor: '#ffffff',
+  },
+  ios: {
+    bundleIdentifier: 'com.fingerprint.e2eapp',
+  },
+  android: {
+    package: 'com.fingerprint.e2eapp',
+  },
+  plugins: [
+    '@config-plugins/detox',
+    [
+      'expo-build-properties',
+      {
+        ios: {
+          extraPods,
+        },
+        android: androidBuildProperties,
+      },
+    ],
+    './plugins/withGradleProperties.js',
+  ],
+}
+
+export default config
--- e2e-app/scripts/compatibility.mjs
+++ e2e-app/scripts/compatibility.mjs
@@ -1,38 +1,27 @@
-import * as fs from 'node:fs'
 import { execSync } from 'node:child_process'
-import path from 'node:path'
-
-const dirname = new URL('.', import.meta.url).pathname
-const appJsonPath = path.resolve(dirname, '../app.json')
 
 /**
  * An object representing metadata for different versions of React Native.
  *
  * Each key in the object represents a specific React Native version, and its
- * associated value contains information about the packages and callbacks
- * required or applicable for that version.
+ * associated value contains information about the packages applicable for that version.
  *
  * Structure:
  * - `packages`: An array of strings specifying the version-specific package dependencies.
- * - `callbacks`: An array of callback functions associated with the version, if any.
  *
  */
 const reactNativeMetadata = {
   0.73: {
     packages: ['expo@50', 'detox@20.17.0', '@config-plugins/detox@7'],
-    callbacks: [disableNewArch, stripExtraBuildProperties],
   },
   0.74: {
     packages: ['expo@51', 'detox@20.20.3', '@config-plugins/detox@8'],
-    callbacks: [disableNewArch, stripExtraBuildProperties],
   },
   0.75: {
     packages: ['expo@51', 'detox@20.20.3', '@config-plugins/detox@8'],
-    callbacks: [disableNewArch, stripExtraBuildProperties],
   },
   0.76: {
     packages: ['expo@51', 'detox@20'],
-    callbacks: [stripExtraBuildProperties],
   },
 }
 
@@ -45,10 +34,6 @@ if (!rnVersion) {
 const metadata = getCompatibilityMetadata(rnVersion)
 
 installPackages(`react-native@${rnVersion}`, ...metadata.packages)
-
-if (metadata.callbacks?.length) {
-  metadata.callbacks.map((cb) => cb())
-}
 
 function installPackages(...packages) {
   execSync(`yarn add ${packages.join(' ')} -D`, {
@@ -64,55 +49,4 @@ function getCompatibilityMetadata(rnVersion) {
   }
 
   return metadata
-}
-
-function readAppJson() {
-  return JSON.parse(fs.readFileSync(appJsonPath, 'utf8'))
-}
-
-function updateAppJson(fn) {
-  const appJson = readAppJson()
-  fn(appJson)
-  fs.writeFileSync(appJsonPath, JSON.stringify(appJson, null, 2))
-}
-
-/**
- * Disables the new architecture in the application's configuration by
- * modifying the configuration file and removing the `newArchEnabled` property.
- */
-function disableNewArch() {
-  console.info('Disabling new architecture')
-
-  updateAppJson((config) => {
-    delete config.expo.newArchEnabled
-  })
-}
-
-/**
- * Removes extra build properties that are not compatible with older expo versions.
- * */
-function stripExtraBuildProperties() {
-  console.info('Stripping extra build properties')
-  updateBuildProperties((config) => {
-    delete config.android.compileSdkVersion
-    delete config.android.targetSdkVersion
-    delete config.android.buildToolsVersion
-  })
-}
-
-function updateBuildProperties(callback) {
-  const PLUGIN_NAME = 'expo-build-properties'
-
-  updateAppJson((config) => {
-    const plugin = config.expo.plugins.find(
-      (plugin) => plugin === PLUGIN_NAME || (Array.isArray(plugin) && plugin[0] === PLUGIN_NAME)
-    )
-    if (!plugin) {
-      return
-    }
-
-    if (Array.isArray(plugin)) {
-      callback(plugin[1])
-    }
-  })
 }
