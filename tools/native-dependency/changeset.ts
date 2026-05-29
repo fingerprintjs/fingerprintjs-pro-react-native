f648f4f3f3c1b77b53c32eee7630f845e0f3b32e
Przemysław Żydek <przemyslawzydek@gmail.com>
2026-05-29 14:53:20 +0200

    refactor: use `getReleasePlan`

--- tools/native-dependency/changeset.ts
+++ tools/native-dependency/changeset.ts
@@ -1,44 +1,7 @@
-import { execSync } from 'node:child_process'
-import * as os from 'node:os'
-import * as path from 'node:path'
-import * as fs from 'node:fs'
-
-type ReleaseType = 'patch' | 'minor' | 'major'
-
-interface ChangesetRelease {
-  name: string
-  type: ReleaseType
-}
-
-interface Changeset {
-  releases: ChangesetRelease[]
-  summary: string
-  id: string
-}
-
-interface Release {
-  name: string
-  type: ReleaseType
-  oldVersion: string
-  changesets: string[]
-  newVersion: string
-}
-
-interface ChangesetStatus {
-  changesets: Changeset[]
-  releases: Release[]
-}
+import getReleasePlan from '@changesets/get-release-plan'
 
 export function getChangesetStatus() {
-  const jsonPath = path.join(os.tmpdir(), `changset-status-${Date.now()}.json`)
-
-  execSync(`changeset status --output ${jsonPath}`)
-
-  try {
-    return JSON.parse(fs.readFileSync(jsonPath, 'utf-8')) as ChangesetStatus
-  } finally {
-    fs.promises.unlink(jsonPath)
-  }
+  return getReleasePlan(process.cwd())
 }
 
 export async function getPendingChangesets() {
@@ -47,7 +10,7 @@ export async function getPendingChangesets() {
     with: { type: 'json' },
   }).then((it) => it.default)
 
-  const changesetStatus = getChangesetStatus()
+  const changesetStatus = await getChangesetStatus()
 
   const changesets = changesetStatus.releases.find((release) => release.name === pkg.name)?.changesets ?? []
 
