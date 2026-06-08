39b9979ade6937fe14035b480e1f4c5c4e79f4b9
Przemysław Żydek <przemyslawzydek@gmail.com>
2026-06-08 16:26:51 +0200

    refactor(e2e-app): remove custom index.ts and update entry point

--- e2e-app/index.ts
+++
@@ -1,6 +0,0 @@
-import registerRootComponent from 'expo/src/launch/registerRootComponent'
-
-import App from './App'
-import { ComponentType } from 'react'
-
-registerRootComponent(App as ComponentType)
