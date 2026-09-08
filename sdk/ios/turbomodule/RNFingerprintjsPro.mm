7955c0adc2d9be49c027d9f0b808561e5216d98e
Przemysław Żydek <przemyslawzydek@gmail.com>
2026-09-08 12:54:31 +0200

    fix(ios): simplify import logic in RNFingerprintjsPro

--- sdk/ios/turbomodule/RNFingerprintjsPro.mm
+++ sdk/ios/turbomodule/RNFingerprintjsPro.mm
@@ -25,24 +25,6 @@ RCT_EXTERN_METHOD(getVisitorData:(NSDictionary *)tag
 // `NativeRNFingerprintjsProSpecJSI` invokes.
 #import <RNFingerprintjsProSpec/RNFingerprintjsProSpec.h>
 
-// Pull in the Swift-generated interface WHEN the build exposes it (CocoaPods, or
-// any SwiftPM config that puts …-Swift.h on this target's header search path).
-// This import is opportunistic and must never hard-fail: under the SwiftPM
-// two-target split some configurations don't expose a Swift target's generated
-// header to a dependent ObjC++ target. That's fine: RCT_EXTERN_MODULE above
-// already declared `@interface RNFingerprintjsPro : NSObject`, which is all the
-// TurboModule category below needs to attach to the class. (The Swift methods
-// are dispatched via the ObjC runtime, not through this header.)
-#if __has_include("RNFingerprintjsPro-Swift.h")
-#import "RNFingerprintjsPro-Swift.h"
-#elif __has_include(<RNFingerprintjsPro/RNFingerprintjsPro-Swift.h>)
-#import <RNFingerprintjsPro/RNFingerprintjsPro-Swift.h>
-#elif __has_include(<RNFingerprintjsProSwift-Swift.h>)
-#import <RNFingerprintjsProSwift-Swift.h>
-#elif __has_include("RNFingerprintjsProSwift-Swift.h")
-#import "RNFingerprintjsProSwift-Swift.h"
-#endif
-
 @interface RNFingerprintjsPro (TurboModule) <NativeRNFingerprintjsProSpec>
 - (std::shared_ptr<facebook::react::TurboModule>)getTurboModule:
     (const facebook::react::ObjCTurboModule::InitParams &)params;
