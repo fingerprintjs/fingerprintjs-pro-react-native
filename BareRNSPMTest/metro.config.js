cecbfb617fb8ee747c1336e3d045a78700b1cfac
Przemysław Żydek <przemyslawzydek@gmail.com>
2026-09-08 13:27:35 +0200

    style: fix linter

--- BareRNSPMTest/metro.config.js
+++ BareRNSPMTest/metro.config.js
@@ -1,8 +1,8 @@
-const { getDefaultConfig } = require('@react-native/metro-config');
+const { getDefaultConfig } = require('@react-native/metro-config')
 
-const { getMetroConfig } = require('../tools/metro');
+const { getMetroConfig } = require('../tools/metro')
 
 /** @type {import('expo/metro-config').MetroConfig} */
-const config = getDefaultConfig(__dirname);
+const config = getDefaultConfig(__dirname)
 
-module.exports = getMetroConfig(config, __dirname);
+module.exports = getMetroConfig(config, __dirname)
